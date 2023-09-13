073114cb6819157e290add719ba29be369183d47
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-09-13 18:22:55 -0400

    initial commit

---
+++ data/skills-data.js
@@ -0,0 +1,12 @@
+const skills = [
+  {text: 'problem-solving', essential: true},
+  {text: 'proficiency in one programming language', essential: true},
+  {text: 'algorithmic thinking', essential: true},
+  {text: 'learning agility', essential: true},
+  {text: 'marketing or sales', essential: false},
+  {text: 'music or artistic', essential: false}
+]
+
+export {
+  skills
+}
